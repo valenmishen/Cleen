2425a95d9b6516265471d43c9deb2d79525c9b4f
Valen <valenishen18@gmail.com>
2022-04-28 16:07:28 +0700

    Initial Commit

---
+++ Cleen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cleen
+//
+//  Created by Valen Meicella Ishen on 28/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
